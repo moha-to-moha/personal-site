ca4bef9bb74440897b32986cda6a11192bfd9a5e
mohato_moha <yonismohamedstudent@gmail.com>
2024-12-22 16:38:27 -0600

    Add files via upload

---
+++ qa.js
@@ -0,0 +1,22 @@
+document.getElementById('questionForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const question = document.getElementById('question').value;
+
+    fetch('https://moha-to-moha.github.io/accessqa.html', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ question: question })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        alert('your question has been sent!');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('there was an error sending your question. sorry!');
+    });
+});
