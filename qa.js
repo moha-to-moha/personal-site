7fea62b332a9e7fb90ef34514f83da74ab6c9878
mohato_moha <yonismohamedstudent@gmail.com>
2024-12-22 17:41:43 -0600

    MAJOR bugfix on qa.js

--- qa.js
+++ qa.js
@@ -2,21 +2,8 @@ document.getElementById('questionForm').addEventListener('submit', function(even
     event.preventDefault();
 
     const question = document.getElementById('question').value;
+    const timestamp = new Date().getTime();
+    localStorage.setItem(`question_${timestamp}`, question);
 
-    fetch('https://moha-to-moha.github.io/personal-site/accessqa.html', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ question: question })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        alert('your question has been sent!');
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        alert('there was an error sending your question. sorry! :(');
-    });
+    alert('your question has been sent.');
 });
