44f17853b8b5ce4e1d06c624569b22353ae7ef98
mohato_moha <yonismohamedstudent@gmail.com>
2024-12-23 18:37:44 -0600

    Delete qa.js

--- qa.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('questionForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const question = document.getElementById('question').value;
-    const timestamp = new Date().getTime();
-    localStorage.setItem(`question_${timestamp}`, question);
-
-    alert('your question has been sent.');
-});
